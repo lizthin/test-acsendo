179ad3f5879703fffabc5f259f395f04c1499160
lizethnunez <lizeth.nsanchez@hrinteactive.co>
2018-12-04 09:59:34 -0500

    section inicial benefits

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  public arrayIcon: any[] = [];
+
+  constructor() {
+
+    this.arrayIcon.push(
+      {
+        id: 1,
+        img: '../../assets/images/competencia.png',
+        description: 'Competencias 360º',
+      },
+      {
+        id: 2,
+        img: '../../assets/images/metas.png',
+        description: 'Metas',
+      },
+      {
+        id: 3,
+        img: '../../assets/images/clima.png',
+        description: 'Clima Laboral',
+      },
+      {
+        id: 4,
+        img: '../../assets/images/pid.png',
+        description: 'Plan Individual de Desarrollo',
+      }
+    )
+
+  }
 
   ngOnInit() {
   }
